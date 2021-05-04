889228b5b653a5b063d219f84cb834686485b57c
John Han <43765484+juseonghan@users.noreply.github.com>
2021-05-03 23:05:03 -0400

    combined thwack_control and disturbance_control

--- thwack_control.ino
+++ thwack_control.ino
@@ -4,14 +4,27 @@
 
 #include <AFMotor.h>
 
+#define LED1 22
+#define LED2 24
+#define LED3 26
+#define LED3 28
+#define LED3 30
+#define LED3 32
+#define LED3 34
+#define LED3 36
+#define LED3 38
 #define stepsPerRevolution 2048
+
+int LED_array[] = {22, 24, 26, 28, 30, 32, 34, 36, 38}; 
 
 AF_Stepper thwack_motor(stepsPerRevolution, 2); 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); 
-  Serial.println("Thwack control"); 
+  for (int i = 0; i < 9; i++) {
+    pinMode(LED_array[i], OUTPUT); 
+  }
 
   thwack_motor.setSpeed(10); 
   
@@ -20,7 +33,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   thwack_motor.step(1024, FORWARD, DOUBLE); 
-  delay(200); 
+  turnOn(); 
+  delay(1000); 
+  turnOff(); 
+  delay(1000); 
+}
+
+void turnOn() {
+  for (int i = 0; i < 9; i++) {
+    digitalWrite(LED_array[i], HIGH); 
+  }
+}
+
+void turnOff() {
+  for (int i = 0; i < 9; i++) {
+    digitalWrite(LED_array[i], LOW); 
+  }
 }
 
 //#include <Wire.h>
